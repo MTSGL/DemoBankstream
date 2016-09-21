775545e8e7dffcefd787475113b20f896b95ec85
Brendon.Frater <brendon.frater@mtsgl.com>
2016-09-21 14:47:55 +1200

    INitial Commit
    Signed-off-by: Brendon.Frater <brendon.frater@mtsgl.com>

---
+++ HelloBankstream/src/main/Class2.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author brendon.frater
+ *
+ */
+public class Class2 {
+
+	/**
+	 * This is the class 2 method
+	 * <p>
+	 * some more information about the method
+	 * <p>
+	 * @return does not return anything
+	 * @param p 
+	 */
+	public static void Class2Method(int p){
+		
+	}
+	
+}
---
+++ HelloBankstream/src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String message = sayHelloToBankstream();
+		
+		System.out.println(message);
+		
+		
+		Class2 myClass = new Class2();
+		
+		
+	}
+
+	public static String sayHelloToBankstream() {
+		return "Hello Bankstream";
+	}
+
+}
